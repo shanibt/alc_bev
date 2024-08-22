0e1eb92d7a8deb2af4530e44c32d5cf9747089e2
shanibt <123358898+shanibt@users.noreply.github.com>
2024-08-22 03:31:39 -0700

    Create game.py
    started with the 16 different personalities

---
+++ game.py
@@ -0,0 +1,23 @@
+const personalities = {
+  # Analysts (INTJ, INTP, ENTJ, ENTP)
+  # Sentinels (ISTJ, ISFJ, ESTJ, ESFJ)
+  # Diplomats (INFJ, INFP, ENFJ, ENFP)
+  # Explorers (ISTP, ISFP, ESTP, ESFP
+
+  vodka: 0,
+  whiskey: 0,
+  hennesey: 0,
+  tequlia: 0,
+  soju: 0,
+  pink-whitney: 0,
+  red-wine: 0,
+  white-wine: 0,
+  champagne: 0,
+  american-beer: 0,
+  taiwanese-beer:0,
+  jungle-juice: 0,
+  twisted-tea: 0,
+  beatbox: 0,
+  buzzball: 0,
+  hard-seltzer: 0
+}
